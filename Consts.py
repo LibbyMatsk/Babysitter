6e099298039f710c474a6f71c17d8a7dd32db9ae
avi hagai <avi.hagai1@gmail.com>
2024-10-09 16:11:20 +0300

    start2

--- Consts.py
+++ Consts.py
@@ -1,9 +1,18 @@
+import pygame
 WINDOW_HEIGHT = 750
 WINDOW_WIDTH = 800
 WHITE = (255, 255, 255)
 LIGHT_BLUE = (173, 216, 230)
 LIGHT_GREEN = (144, 238, 144)
-FONT_NAME = "Calibri"
+FONT_NAME = "David"
 
-BACKGROUND_COLOR = (255, 255, 255)
-HELLO_COLOR = (89, 89, 89)
+
+BACKGROUND_COLOR = (255,204,255)
+HELLO_COLOR = (32, 32, 32)
+
+VOLUNTEER_IMAGE = pygame.image.load('volunteer.jpg')
+VOLUNTEER_IMAGE = pygame.transform.smoothscale(VOLUNTEER_IMAGE, (70, 70))
+LOGO_IMAGE = pygame.image.load('logo2.png')
+LOGO_IMAGE = pygame.transform.smoothscale(LOGO_IMAGE, (100, 90))
+PARENT_IMAGE = pygame.image.load('download.png')
+PARENT_IMAGE = pygame.transform.smoothscale(PARENT_IMAGE, (70, 50))
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 import sys
 import entering_screen
 
+current_user = ""
 
 def main():
     pygame.init()
@@ -10,11 +11,22 @@ def main():
         entering_screen.draw_welcome_screen()
 
 
-
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:  # Left mouse button
+                    if entering_screen.create_parent_button().collidepoint(event.pos):
+                        print("Parent selected")
+
+                        current_user = "Parent"
+                    elif entering_screen.create_volunteer_button().collidepoint(event.pos):
+                        print("volunteer selected")
+                        current_user = "volunteer"
+
+
 
 
 
